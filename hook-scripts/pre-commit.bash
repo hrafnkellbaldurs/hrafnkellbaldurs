d7a6b3fba4df8d80b30a095b6475dd10d51ed62f
Hrafnkell Baldursson <kotkarl@gmail.com>
2018-12-04 01:01:54 +0000

    Changed hook script

--- hook-scripts/pre-commit.bash
+++ hook-scripts/pre-commit.bash
@@ -2,10 +2,11 @@
 
 echo "Running pre-commit hook"
 ./hook-scripts/run-format.bash
-./hook-scripts/build-storybook.bash
 ./hook-scripts/run-tests.bash
 # $? stores exit value of the last command
 if [ $? -ne 0 ]; then
  echo "Tests must pass before commit!"
  exit 1
 fi
+
+./hook-scripts/build-storybook.bash
