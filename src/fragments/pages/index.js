c0c54363fd98b4ce787bfb24add31a34d513e11b
Hrafnkell Baldursson <kotkarl@gmail.com>
2018-12-05 16:45:28 +0000

    #34 Fixed Experience mobile view

--- src/fragments/pages/index.js
+++ src/fragments/pages/index.js
@@ -57,8 +57,8 @@ export const IndexPageFragment = graphql`
                     },
                     title,
                     subtitle,
-                    startDate(formatString: "MMMM YYYY"),
-                    endDate(formatString: "MMMM YYYY"),
+                    startDate(formatString: "MMM YYYY"),
+                    endDate(formatString: "MMM YYYY"),
                     description
                 }
             }
@@ -84,8 +84,8 @@ export const IndexPageFragment = graphql`
                     },
                     title,
                     subtitle,
-                    startDate(formatString: "MMMM YYYY"),
-                    endDate(formatString: "MMMM YYYY"),
+                    startDate(formatString: "MMM YYYY"),
+                    endDate(formatString: "MMM YYYY"),
                     description
                 }
             }
