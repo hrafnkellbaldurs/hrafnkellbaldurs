cd248f8aa03b13b0f3b8cd41490c3261e8be93ed
Hrafnkell Baldursson <kotkarl@gmail.com>
2018-12-05 18:11:05 +0000

    #34 Fixed mobile view for SkillGrid

--- src/components/SkillBadge/SkillBadge.js
+++ src/components/SkillBadge/SkillBadge.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import styles from './SkillBadge.module.scss'
 import * as R from 'ramda'
+import { createGlobalLocalClassnames } from '../../scripts/utils'
+import classnames from 'classnames'
+
+const globalLocalClassnames = R.partial(createGlobalLocalClassnames, [styles])
 
 const SkillBadge = ({ id, logo = {}, name, shortName }) => {
     logo = R.defaultTo({}, logo)
 
-    const LogoImage = () => <img className={ styles.itemLogo } src={ logo.publicURL }/>
-    const FallbackImage = () => <span className={ styles.fallbackLogo }>{ shortName } </span>
+    const LogoImage = () => (
+        <img
+            className={ styles.logo }
+            src={ logo.publicURL }
+        />
+    )
+    const FallbackImage = () => (
+        <span
+            className={ styles.fallbackLogo }
+        >
+            { shortName }
+        </span>
+    )
 
     const image = R.ifElse(
         () => !R.isNil(logo.publicURL),
@@ -14,10 +29,25 @@ const SkillBadge = ({ id, logo = {}, name, shortName }) => {
         FallbackImage
     )()
 
+    const containerProps = {
+        key: id,
+        className: classnames(
+            globalLocalClassnames('SkillBadge')
+        )
+    }
+
+    const itemLabelProps = {
+        className: classnames(
+            globalLocalClassnames('itemLabel')
+        )
+    }
+
     return (
-        <div className={ styles.container }>
-            { image }
-            <div className={ styles.itemLabel }>
+        <div { ...containerProps }>
+            <div className={ styles.logoContainer }>
+                { image }
+            </div>
+            <div { ...itemLabelProps }>
                 { name }
             </div>
         </div>
