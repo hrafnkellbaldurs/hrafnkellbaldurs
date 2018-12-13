d7a1c3fda7b38588368883cc61c80f459442efd0
Hrafnkell Baldursson <kotkarl@gmail.com>
2018-12-13 16:23:34 +0000

    #61 Styled 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,45 @@
 import React from 'react'
 import { LayoutContainer } from '../components/Layout'
+import Section from '../components/Section'
+import Link from '../components/Link'
+import '../styles/pages/404/main.scss'
+import { ReactComponent as HomeIcon } from '../assets/icons/home-1.svg'
+import { ReactComponent as ErrorIcon } from '../assets/icons/error.svg'
 
-const NotFoundPage = () => {
-    return (
-        <LayoutContainer>
-            <h1>NOT FOUND</h1>
-            <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-        </LayoutContainer>
-    )
+class NotFoundPage extends React.Component {
+    state = {
+        height: 300
+    }
+    componentDidMount() {
+        const headerHeight = document.querySelector('header').clientHeight
+        const footerHeight = document.querySelector('footer').clientHeight
+        const height = window.innerHeight - headerHeight - footerHeight
+        this.setState({ height })
+    }
+
+    render() {
+        return (
+            <LayoutContainer>
+                <Section id="content404" style={ { height: `${ this.state.height }px` } }>
+                    <div className="row container">
+                        <div className="message-container accent-underline">
+                            <div className="left-column">
+                                <h1>Not found</h1>
+                                <span>Sorry, this page does not exist</span>
+                            </div>
+                            <div className="right-column">
+                                <ErrorIcon/>
+                            </div>
+                        </div>
+                        <Link className="button icon-right go-home" to="/#home">
+                            Back to home
+                            <HomeIcon/>
+                        </Link>
+                    </div>
+                </Section>
+            </LayoutContainer>
+        )
+    }
 }
 
 export default NotFoundPage
