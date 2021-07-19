b729c14b42fbd2bb4d421c87d2abe6d2bc030fe8
Hrafnkell Baldursson <kotkarl@gmail.com>
2021-07-19 22:44:23 +0000

    feat(storybook): Gatsby support storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,4 +4,30 @@ module.exports = {
   core: {
     builder: 'webpack5',
   },
+  webpackFinal: async (config) => {
+    // transpile Gatsby module because Gatsby includes un-transpiled ES6 code.
+    // NOTE: config.module.rules[0].exclude = [/node_modules\/(?!(gatsby)\/)/] did not work, used include instead
+    config.module.rules[0].include.push(/node_modules\/gatsby\//);
+    // use babel-plugin-remove-graphql-queries to remove static queries from components when rendering in storybook
+    config.module.rules[0].use[0].options.plugins.push(
+      require.resolve('babel-plugin-remove-graphql-queries'),
+    );
+    return config;
+  },
+  // https://storybook.js.org/docs/react/configure/typescript
+  typescript: {
+    // Optionally run fork-ts-checker-webpack-plugin
+    check: false,
+    // Options to pass to fork-ts-checker-webpack-plugin if it's enabled
+    checkOptions: {},
+    // Which react docgen processor to run:
+    // "react-docgen-typescript", "react-docgen", false
+    reactDocgen: 'react-docgen-typescript',
+    // Options to pass to react-docgen-typescript-plugin if react-docgen-typescript is enabled.
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) =>
+        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
+    },
+  },
 };
